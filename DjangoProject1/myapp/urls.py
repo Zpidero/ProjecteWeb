d1a0db696cab0476080811dfb46d88ef92b06031
JoinedRobbin1887 <eduterbo@gmail.com>
2026-03-31 20:47:16 +0200

    feat: Apply FutDraft function only for 433 formation

--- DjangoProject1/myapp/urls.py
+++ DjangoProject1/myapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('base', views.base, name='base'),
+    path('', views.home, name='home'),
+    path('random-players/', views.get_random_players, name='random_players'),
+    path('game/', views.game_view, name='game'),
 ]
