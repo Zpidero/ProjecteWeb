758e2aff0d188ef9fe5fa3f39e0a7e0f3a553e1e
JoinedRobbin1887 <eduterbo@gmail.com>
2026-04-01 19:23:31 +0200

    feat: Work by positions

--- DjangoProject1/myapp/views.py
+++ DjangoProject1/myapp/views.py
@@ -10,11 +10,20 @@ def home(request):
     return render(request, "myapp/home.html")
 
 def get_random_players(request):
+    position = request.GET.get("position", "")
+
     with urllib.request.urlopen('http://api:5000/all') as response:
         data = json.loads(response.read().decode())
+
     all_players = list(data.values())
-    random_players = random.sample(all_players, 5)
-    return JsonResponse(random_players, safe=False)  # devuelve solo los 5 jugadores como JSON
+    if position:
+        filtered_players = [p for p in all_players if p.get("Position", "").upper() == position]
+    else:
+        filtered_players = all_players
+
+    num_to_take = min(5, len(filtered_players))
+    random_players = random.sample(filtered_players, num_to_take) if num_to_take > 0 else []
+    return JsonResponse(random_players, safe=False)
 
 def game_view(request):
     return render(request, 'myapp/game.html')
